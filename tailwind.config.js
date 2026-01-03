4f0469da94dddb085470bca6f01bda86372b5337
abhi-shah-mech <abhishekshah7@gmail.com>
2026-01-03 16:30:27 -0600

    customizing cupcake to cupcake_structured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import themes from "daisyui/src/theming/themes";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["cupcake"],
+    themes: [
+      {
+        cupcake_structured: {
+          ...themes.cupcake,
+
+          // Darken surface separation
+          "base-300": "#d2cbc5",
+        },
+      },
+    ],
   },
 };
