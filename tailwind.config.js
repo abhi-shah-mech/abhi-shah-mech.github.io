3a197449bab0e588eee676b9422f7367972cf854
abhi-shah-mech <abhishekshah7@gmail.com>
2026-01-03 16:37:12 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-import themes from "daisyui/src/theming/themes";
-
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        cupcake_structured: {
-          ...themes.cupcake,
-
-          // Darken surface separation
-          "base-300": "#d2cbc5",
-        },
-      },
-    ],
+    themes: ["cupcake"],
   },
 };
